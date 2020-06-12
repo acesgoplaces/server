a5bd66e72bf1f718216ec46b586e3dd1d4706dca
Huey <leejinhuey@gmail.com>
2020-06-13 00:56:42 +0800

    backend

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return "home page"
-
-@app.route('/p/<string:id>')
-def show_page(id): # this is the link sent via SMS
-  return render_template('link.html', id=id)
+@app.route('/api/coords', methods=["HEAD", "POST"])
+def receive_coords():
+  return
