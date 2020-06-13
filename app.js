3471f3d315bc8c5a2ef67645736ce65a4714e19a
Huey <leejinhuey@gmail.com>
2020-06-13 13:44:43 +0800

    more code

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import multer from 'multer'
+import fs from 'fs'
 
 import Db from './lib/Db'
+import S3 from './lib/S3'
 
 const upload = multer({ dest: 'uploads/' })
 
@@ -20,6 +22,11 @@ app.get(`/`, (req, res) => res.send(
   Db.addLocation({ userId: `1`, location: `s` }))
 )
 
+app.post(`/new`, (req, res) => {
+  const userId = Db.newUser()
+  return res.send(userId)
+})
+
 app.post(`/location`, (req, res) => {
   // const userId = req.header(`User-Id`)
   const userId = `1`
@@ -28,21 +35,33 @@ app.post(`/location`, (req, res) => {
   return res.json(Db.addLocation({ location, userId }))
 })
 
-app.post(`/orientation`, (req, res) => {
+app.put(`/orientation`, (req, res) => {
   const { orientation } = req.body
   return res.json(Db.setOrientation({ orientation, userId }))
 })
 
-app.post(`/battery`, (req, res) => {
+app.put(`/battery`, (req, res) => {
 
 })
 
-app.post(`/photo`, upload.single('photo'), (req, res) => {
-  console.log(req.file)
-  if (!req.file) {
+app.post(`/photo`, upload.single('photo'), async (req, res) => {
+  if (!req.file || req.file.length === 0) {
     return res.status(400).send(`No file uploaded`)
   }
-  return res.send(`ok`)
+
+  const { file } = req
+  const fileData = fs.readFileSync(file.path)
+
+  const url = await S3.uploadFile({
+    fileName: file.filename,
+    body: fileData
+  })
+
+  await Db.addPhoto({
+    userId: `1`,
+    photoURL: url,
+  })
+  return res.send(url)
 })
 
 app.listen(
